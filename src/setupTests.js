74f71006d9c94707edfec4df93025bce5c28e71c
Abe Yoshida <abeyoshida@gmail.com>
2022-11-04 12:47:49 -0400

    Install Enzyme and setupTests.js for testing

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,5 +1,9 @@
-// jest-dom adds custom jest matchers for asserting on DOM nodes.
-// allows you to do things like:
-// expect(element).toHaveTextContent(/react/i)
-// learn more: https://github.com/testing-library/jest-dom
-import '@testing-library/jest-dom';
+import Enzyme from 'enzyme';
+import Adapter from 'enzyme-adapter-react-18';
+
+/**
+ * Whenever Jest runs it looks for "setupTests.js" inside of the /src
+ * folder and executes its contents before any test file.  Now we can 
+ * use Enzyme anywhere inside of our project.
+ */
+Enzyme.configure({adapter: new Adapter()});
